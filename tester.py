4a834a719c0f186b281ad38b606177fc7affd41a
chrispham <66860671+chrispham45@users.noreply.github.com>
2021-03-31 10:59:55 +1300

    slot machine complete

--- tester.py
+++ tester.py
@@ -1,9 +1,32 @@
 import random
+multiplier = 0
 values = ['orange', 'banana', 'grape', 'coin']
+emoji_dict = {'orange':'🍊', 'banana':'🍌', 'grape':'🍇', 'coin':'💰'}
 output = []
 for i in range(3):
-    output += [random.choice(values)]
+ output += [random.choice(values)]
+        
+if output[0] == output[1] and output[0] == output[2]:
+    if output[0] == 'coin':
+        multiplier = 5
+    else:
+            multiplier = 3
+elif output[0] == output[1] or output[1] == output[2] or output[0] == output[2]:
+    if output.count('coin') == 2:
+        multiplier = 2.5
+    else:
+        multiplier = 2
+elif 'coin' in output:
+    multiplier = 0.5
+else:
+    multiplier = 0
+
 
 print(output)
-print(output.count('coin'))
+for i in range(len(output)):
+    output[i] = emoji_dict[output[i]]
+
+print(output)
+print(multiplier)
+
         
